01590b7adc41d253fac88ecebed673645fa40b4d
John Donoghue <john.donoghue@ieee.org>
2023-05-23 11:45:50 -0400

    * inst/vco.m: fix copyright header date

--- inst/vco.m
+++ inst/vco.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2029-2023 The Octave Project Developers
+## Copyright (C) 2019-2023 The Octave Project Developers
 ##
 ## This program is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
