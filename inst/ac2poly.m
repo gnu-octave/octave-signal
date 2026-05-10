333e6fd15c5848688bfa36ba61e47d48086a39bd
phychi <chadholton@qq.com>
2026-05-10 20:47:13 +0800

    Add ac2poly function
    Introduce `ac2poly`: a new function that converts an autocorrelation sequence r into a prediction polynomial a using the Levinson–Durbin recursion . The function returns an optional prediction error e.

---
+++ inst/ac2poly.m
@@ -0,0 +1,54 @@
+## Copyright (C) 2026 Tang Chonghao <chadholton@qq.com>
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; see the file COPYING. If not, see
+## <https://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## @deftypefn  {Function File} {@var{a} =} ac2poly (@var{r})
+## @deftypefnx {Function File} {[@var{a}, @var{e}] =} ac2poly (@var{r})
+## Convert autocorrelation sequence to prediction polynomial.
+##
+## Compute the prediction filter coefficients @var{a} from the autocorrelation
+## sequence @var{r} using the Levinson-Durbin recursion. The second optional
+## output @var{e} returns the final prediction error power.
+##
+## This function directly calls @code{levinson}.
+##
+## @seealso{levinson, poly2ac}
+## @end deftypefn
+
+function [a, e] = ac2poly (r)
+
+  if (nargin != 1)
+    print_usage ();
+  endif
+
+  if (!isnumeric (r) || !(isvector (r) || ismatrix (r)))
+    error ("ac2poly: R must be a numeric vector or matrix.");
+  endif
+
+  [a, e] = levinson (r);
+
+endfunction
+
+%!error ac2poly()
+%!error ac2poly(1,2)
+%!error <numeric vector or matrix> ac2poly("abc")
+%!error <numeric vector or matrix> ac2poly({1,2,3})
+
+%!test
+%! r = [3, -1, -4, 3, 1, -2];
+%! [a, e] = ac2poly(r);
+%! assert (a, [1,-2.8281,-1.9687,-0.1875,1.0312,3.6719], 1e-4);
+%! assert (e, 6.8281, 1e-4);
